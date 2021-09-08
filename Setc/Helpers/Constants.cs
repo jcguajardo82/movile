d0a89ea3c13cdeb8e6aebccca811cef255e4b643
Héctor Reyes on Lenovo <hector@dyasoftwaresolutions.onmicrosoft.com>
2021-09-08 17:01:58 -0500

    cambio a URL de produccion v 01.0.1

--- Setc/Helpers/Constants.cs
+++ Setc/Helpers/Constants.cs
@@ -3,6 +3,6 @@
     public static class Constants
     {
         public const string GestorUrl = "http://gestorinvsc.supercity.com.mx/";
-        public const string SectUrl = "https://sorianaappomsmsqa.azurewebsites.net";
+        public const string SectUrl = "https://sorianaappomsmsprd.azurewebsites.net";
     }
 }
